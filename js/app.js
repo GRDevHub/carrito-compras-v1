b7dee25acca86adf6e3bb05ec9d2164d5135d2e3
Gabriel Riveros <velo02.gr@gmail.com>
2024-07-05 11:30:51 -0400

    Agregar bootstrap y actualizar estructura HTML

--- js/app.js
+++ js/app.js
@@ -1,12 +1,22 @@
-
-
+const botones = document.querySelectorAll('.btn-primary');
+const template = document.querySelector('#template');
+const contenedor = document.querySelector('#contenedor')
 const frutas = [
     {
         name: 'manzana',
         img: "🍎"
+    },
+    {
+        name: 'naranja',
+        img: '🍊'
+    },
+    {
+        name: 'banana',
+        img: '🍌'
     }
 ]
 
 frutas.forEach( item => {
-
+    console.log(`Articulo: ${item.name} \nImg: ${item.img}`);
 });
+
