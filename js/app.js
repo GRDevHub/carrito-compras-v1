37968d0180797be0d7e7e97255f52f5972dec7fd
Gabriel Riveros <velo02.gr@gmail.com>
2024-07-06 12:29:06 -0400

    Carrito de compras V1 terminado agregar al carrito sin mostrar en la web de forma dinamica

--- js/app.js
+++ js/app.js
@@ -1,22 +1,32 @@
 const botones = document.querySelectorAll('.btn-primary');
 const template = document.querySelector('#template');
-const contenedor = document.querySelector('#contenedor')
-const frutas = [
-    {
-        name: 'manzana',
-        img: "🍎"
-    },
-    {
-        name: 'naranja',
-        img: '🍊'
-    },
-    {
-        name: 'banana',
-        img: '🍌'
-    }
-]
+const contenedor = document.querySelector('#contenedor');
 
-frutas.forEach( item => {
-    console.log(`Articulo: ${item.name} \nImg: ${item.img}`);
+// Carrito
+const carrito = {};
+
+// Agregar producto carrito
+const addProducto = (e) => {
+    // console.log(e.target.dataset.fruta)
+    // producto
+    const producto = {
+        nombre: e.target.dataset.fruta,
+        id: e.target.dataset.fruta,
+        cantidad: 1,
+    }
+    // console.log(producto)
+    // agregar producto al carrito
+    carrito[producto.nombre] = producto;
+
+    console.log(carrito);
+}
+
+// mostrar el carrito en la web
+const viewProducto = () => {
+
+}                                                                           
+
+botones.forEach( item => {
+    item.addEventListener('click', addProducto);
 });
 
